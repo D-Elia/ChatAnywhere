6db040a216ffe81c061105a2d883352fd70b3904
D-Elia <rdeliajohn84@gmail.com>
2022-08-04 22:11:47 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import { Container, Row, Col } from "react-bootstrap";
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
+// import { useEffect } from 'react';
+// import { ChatProvider } from 'context';
+// import 'semantic-ui-css/semantic.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+// import { Routes, Route, useHistory } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+// import Navbar from "./components/Navbar"
+// import { useAuth, useResolved } from 'hooks';
+import Login from './components/Login';
+import Signup from './components/Signup';
 // import ForgotPassword from "./components/ForgotPassword";
-import ProtectedRoute from "./components/ProtectedRoute";
-import { UserAuthContextProvider } from "./context/UserAuthContext";
+import ProtectedRoute from './components/ProtectedRoute';
+import { UserAuthContextProvider } from './context/UserAuthContext';
 
+// export const App = () => {
+//   const history = useHistory();
+//   const { authUser } = useAuth();
+//   const authResolved = useResolved(authUser);
+
+//   // If the user is logged in it will prevent the
+//   // user from seeing the login/signup screens
+//   // by always redirecting to chat on auth change.
+//   useEffect(() => {
+//     if (authResolved) {
+//       history.push(!!authUser ? '/' : '/login');
+//     }
+//   }, [authResolved, authUser, history]);
+// };
 function App() {
   return (
-    <Container style={{ width: "400px" }}>
+    <Container style={{ width: '400px' }}>
       <Row>
         <Col>
           <UserAuthContextProvider>
             <Routes>
+              {/* <Navbar> */}
               <Route
                 path="/home"
                 element={
@@ -26,6 +47,7 @@ function App() {
               <Route path="/" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
               {/* <Route path="/forgotpassword" element={<ForgotPassword />} /> */}
+              {/* </Navbar> */}
             </Routes>
           </UserAuthContextProvider>
         </Col>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -12,7 +12,7 @@ root.render(
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
